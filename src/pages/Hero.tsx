16742f4202b286b5943b67e4b19563ad544a801b
Pablo Nicolas  Segato <pablo.segato47@gmail.com>
2026-03-06 19:47:36 +0100

    Sigo mejorando el CSS ya que encontre un monton de errores y vi que es mas efectivo sacar los modulos y hacerlo de una forma mas sencilla, esto me ayuda a mejorar el estilo de la pagina

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,24 +1,22 @@
-import styles from "../modules/Hero.module.css";
-
+import "./Hero.css";
 export const Hero = () => {
   return (
-    <section>
-      <div className={styles.container}>
-        <h3>
-          Bienvenidos al 3er <strong> MICROCLIMA </strong> del mundo
-        </h3>
-        <h1>
-          Tu Refugio en las <br />
-          <strong> Sierras de Merlo</strong>
-        </h1>
-        <p>
-          Descrubrí la serenidad del tercer microclima del mundo y el confort de
-          <br />
-          un hogar pensado paratu descanso absoluto en San Luis, Argentina
-        </p>
-        <div className={styles.container_buttons}>
-          <button>Consultar Disponibilidad</button>
-          <button>Ver Galeria</button>
+    <section className="section_container">
+      <div className="container">
+        <div className="container_text">
+          <h3>
+            Bienvenidos al 3er <strong> MICROCLIMA </strong> del mundo
+          </h3>
+          <h1>
+            Tu Refugio en las <br />
+            <strong> Sierras de Merlo</strong>
+          </h1>
+          <p>
+            Descrubrí la serenidad del tercer microclima del mundo y el confort
+            de
+            <br />
+            un hogar pensado paratu descanso absoluto en San Luis, Argentina
+          </p>
         </div>
       </div>
     </section>
